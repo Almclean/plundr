9b0f892738bfb4d20df146b54d4547a3c196258c
Alistair McLean <al.mclean@gmail.com>
2013-01-05 12:26:42 +0000

    Linked price service to classifier

--- PriceService.py
+++ PriceService.py
@@ -4,21 +4,19 @@ import urllib2
 
 
 class PriceService(object):
-    def getPriceFor(self, stocks=None):
-        if stocks is None:
+    def getPriceFor(self, stock=None):
+        if stock is None:
             return None
         else:
-            for stock in stocks:
-                base_url = """http://download.finance.yahoo.com/d/quotes.csv?"""
-                format_url = """&f=nsl1op&e.csv"""
-                stock_url = "s=" + str(stock)
-                final_url = base_url + stock_url + format_url
-                print final_url
-                f = urllib2.urlopen(final_url)
-                return f.read()
+            base_url = """http://download.finance.yahoo.com/d/quotes.csv?"""
+            format_url = """&f=nsl1op&e.csv"""
+            stock_url = "s=" + str(stock)
+            final_url = base_url + stock_url + format_url
+            f = urllib2.urlopen(final_url)
+            return f.read()
 
 if __name__ == '__main__':
     p = PriceService()
-    print p.getPriceFor(['GOOG'])
-    print p.getPriceFor(['MSFT'])
-    print p.getPriceFor(['AAPL'])
+    print p.getPriceFor('GOOG')
+    print p.getPriceFor('MSFT')
+    print p.getPriceFor('AAPL')
--- StreamWatchHandler.py
+++ StreamWatchHandler.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
 import tweepy
+from PriceService import PriceService
 
 
 class StreamHandler(tweepy.StreamListener):
     def __init__(self, statusListener):
         super(StreamHandler, self).__init__()
         self.statusListener = statusListener
+        self.price_service = PriceService()
 
     def on_status(self, status):
         try:
             usr = status.author.screen_name.strip()
             txt = status.text.strip()
-            print 'Tweet consists of %s' % str(self.statusListener.classify(txt))
-            # print '%s => %s' % (usr, txt)
+            result_set = self.statusListener.classify(txt)
+            print 'Tweet consists of %s' % str(result_set)
+            for stock in result_set:
+                print self.price_service.getPriceFor(stock)
         except Exception as e:
             print e
 
